fdbe73f3a797ab0327f54bc75ec6a83129ddecdb
me@williambarstad.com <me@williambarstad.com>
2021-10-16 12:09:38 -0700

    Changed homepage.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const hpMessage = screen.getByText(/Formula 1 Data Browser/i);
+  expect(hpMessage).toBeInTheDocument();
 });
