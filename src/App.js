e291a3617a8d29921b41b661cb70042719ce1936
me@williambarstad.com <me@williambarstad.com>
2021-10-16 18:42:00 -0700

    Amplify Auth installed. Needs config, broke test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-
-
 import logo from './logo.svg';
 import './App.css';
-
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -10,9 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Formula 1 Data Browser</h1>
+        <h2>We now have Auth!</h2>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
